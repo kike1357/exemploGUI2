e04395f58a9c4581006c89b64f80b906949feacf
epastorizalorenzo <epastorizalorenzo@atanasoff26.danielcastelao.org>
2016-04-07 12:31:58 +0200

    commit github

---
+++ src/exemplogui1/ExemploGui1.java
@@ -0,0 +1,19 @@
+package exemplogui1;
+
+import javax.swing.JFrame;
+
+
+public class ExemploGui1 {
+
+    
+    public static void main(String[] args) {
+        JFrame marco = new JFrame("primeiro programa Gui1");
+        marco.setSize(600,800);
+        marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        marco.setVisible(true);
+        
+    }                      
+    }
+    
+
+
